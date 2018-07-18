77f4966a021a940c096e4bf083734950478cc9d9
Daniel Sabanski <daniel.sabanski@gmail.com>
2018-07-18 13:44:22 +0000

    first commit

--- models/user.js
+++ models/user.js
@@ -3,11 +3,13 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 
 var UserSchema = new mongoose.Schema({
-    username: String,
+    username: {type: String , unique: true , required: true},
     password: String,
     firstName: String,
     lastName: String,
-    email: String,
+    email: {type: String , unique: true , required: true},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     avatar: {type: String , default: 'https://as2.ftcdn.net/jpg/01/17/95/91/500_F_117959178_mOp22kjhdhWdoSoePHPafN7GLUYyvFNY.jpg'},
     isAdmin: {type: Boolean , default: false}
 });
